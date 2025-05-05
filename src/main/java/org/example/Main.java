628f6dae60c9c71316e157beebd40917aea2ca02
kenneth <kennethchau@cpp.edu>
2025-05-05 16:16:00 -0700

    added diagonal check

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -105,6 +105,12 @@ public class Main extends Application {
                 return true;
             }
         }
+
+        // added diagonal check
+        if (checkLine(board[0][0], board[1][1], board[2][2]) ||
+                checkLine(board[0][2], board[1][1], board[2][0])) {
+            return true;
+        }
         return false;
     }
 
